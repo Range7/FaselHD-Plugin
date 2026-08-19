5b68e12f658abef9183fb5e62cb4b853a72ae86f
Range7 <96634855+Range7@users.noreply.github.com>
2026-08-19 03:35:15 +0300

    Create config.js

---
+++ src/krmzy/config.js
@@ -0,0 +1,10 @@
+// src/krmzy/config.js
+
+export const CONFIG = {
+    BASE_URL: 'https://krmzi.net',
+    TMDB_API_KEY: '15d2850027b7744312013263e12089b3',
+    HEADERS: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Referer': 'https://krmzi.net/'
+    }
+};
